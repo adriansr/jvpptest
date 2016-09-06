244ad417d59dc73b9af81bf793a8cd3493fc1c8f
Adrian Serrano <adrian@midokura.com>
2016-09-06 13:03:58 +0200

    base

---
+++ src/main/scala/adriansr/jvpptest/Main.scala
@@ -0,0 +1,48 @@
+package adriansr.jvpptest
+
+import scala.util.Try
+
+import org.openvpp.jvpp._
+import org.openvpp.jvpp.callback.JVppCallback
+import org.openvpp.jvpp.dto.AfPacketCreate
+
+/**
+  * Created by adrian on 06/09/16.
+  */
+
+
+object Main {
+
+    val ConnectionName = "test"
+    val HostIfName = "dev"
+    val MacAddr = "62:44:4e:de:89:c1"
+
+    val connectCallback = new JVppCallback {
+        override def onError(e: VppCallbackException): Unit = {
+            println("Connect callback onError: $e")
+        }
+    }
+
+    def timeOp[T](name: String, op: => T): Try[T] = {
+        val startTime = System.nanoTime()
+        val retval = Try { op }
+        val took = (System.nanoTime - startTime) / 1000000
+        if (retval.isSuccess) {
+            println(s"Operation $name completed in $took ms with value ${retval.get}")
+        } else {
+            println(s"Operation $name failed in $took ms with exception ${retval.failed.get}")
+        }
+        retval
+    }
+    def main(args: Array[String]): Unit = {
+        val lib: JVpp = new JVppImpl(new VppJNIConnection(ConnectionName))
+        require(timeOp("connect", lib.connect(connectCallback)).isSuccess)
+
+        val msg = new AfPacketCreate
+        msg.hostIfName = HostIfName.toCharArray.map( _.toByte )
+        msg.hwAddr = MacAddr.toCharArray.map( _.toByte )
+        msg.useRandomHwAddr = 0
+        val r = timeOp("afPacketCreate", lib.afPacketCreate(msg))
+        require(r.isSuccess)
+    }
+}
